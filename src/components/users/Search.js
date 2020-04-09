09b3e720186f9e132d1e7d08d28ce4c39c8aceda
ogii <ordinallx@gmail.com>
2020-04-09 19:41:33 +0900

    Added routing and user pages

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,12 +8,19 @@ export class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
   };
 
   onChange = (e) => {
@@ -21,6 +28,7 @@ export class Search extends Component {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -33,10 +41,17 @@ export class Search extends Component {
           />
           <input
             type="submit"
-            value="search"
+            value="Search"
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button
+            className="btn btn-light btn-block"
+            onClick={clearUsers}
+          >
+            Clear
+          </button>)}
       </div>
     );
   }
